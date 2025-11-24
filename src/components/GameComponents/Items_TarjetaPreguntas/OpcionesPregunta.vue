8ffed319fcca6c5bd29170f6807992c433ec7747
pablogarridok <pablogarrub@monlau.com.com>
2025-11-24 18:35:37 +0100

    Se comprueba las respuesta

--- src/components/GameComponents/Items_TarjetaPreguntas/OpcionesPregunta.vue
+++ src/components/GameComponents/Items_TarjetaPreguntas/OpcionesPregunta.vue
@@ -1,9 +1,13 @@
 <template>
   <div v-if="respuestasActuales.length > 0" id="opciones">
     <label v-for="(r, index) in respuestasActuales" :key="index">
-      <input type="radio" :value="r" name="respuesta" />
+      <input type="radio" :value="r" name="respuesta" v-model="respuestaSeleccionada" />
       {{ r }}
     </label>
+  </div>
+
+  <div id="btn-comprobar">
+    <button @click="comprobarRespuesta">Comprobar</button>
   </div>
 </template>
 
@@ -12,22 +16,36 @@ import { ref, watch } from 'vue'
 import { usePreguntasStore } from '../../../stores/preguntasStore'
 
 const preguntasStore = usePreguntasStore()
-const respuestasActuales = ref([])
 
-// Función para actualizar las respuestas de la canción actual
+const respuestasActuales = ref([])
+const respuestaSeleccionada = ref(null)
+
+// --- cargar respuestas cuando cambie la canción actual ---
 function actualizarRespuestas() {
   const cancion = preguntasStore.canciones[preguntasStore.indice]
   respuestasActuales.value = cancion?.respuestas || []
+  respuestaSeleccionada.value = null // limpiar selección
 }
 
-// Observar cambios en canciones o índice
-watch(
-  () => [preguntasStore.canciones, preguntasStore.indice],
-  () => {
-    actualizarRespuestas()
-  },
-  { immediate: true }, // para ejecutar al montar el componente
-)
+watch(() => [preguntasStore.canciones, preguntasStore.indice], actualizarRespuestas, {
+  immediate: true,
+})
+
+// --- comprobar respuesta ---
+function comprobarRespuesta() {
+  const cancion = preguntasStore.canciones[preguntasStore.indice]
+
+  if (!respuestaSeleccionada.value) {
+    alert('Selecciona una respuesta 😅')
+    return
+  }
+
+  if (respuestaSeleccionada.value === cancion.correcta) {
+    alert('✔ ¡Correcto!')
+  } else {
+    alert('❌ Incorrecto')
+  }
+}
 </script>
 
 <style scoped></style>
